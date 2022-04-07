2db37a76264e36fec03cb58e571bedc8d7143360
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-04-07 22:03:16 +0545

    Fonts Added and FontManager
    Different fonts is added and font manager is  created.

---
+++ tut_app/lib/presentation/fonts_manager.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = "Montserrat";
+}
+
+class FontWeightmanager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSizeManager {
+  static const double s1 = 12.0;
+  static const double s2 = 14.0;
+  static const double s3 = 16.0;
+  static const double s4 = 17.0;
+  static const double s5 = 18.0;
+  static const double s6 = 20.0;
+}
