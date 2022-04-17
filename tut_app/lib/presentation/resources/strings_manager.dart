da789062fdd2b70271e2c076a62180ce6b2855f2
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-04-17 13:45:27 +0545

    Adding Assets for Onboarding Screen
    Assets is added for onboarding screen.

---
+++ tut_app/lib/presentation/resources/strings_manager.dart
@@ -0,0 +1,17 @@
+class AppStrings{
+  static const String noRouteFound = "No route found";
+  static const String onBoardingTitle1 = "SEE THE BEST COURSE #1";
+  static const String onBoardingTitle2 = "SEE THE BEST COURSE #2";
+  static const String onBoardingTitle3 = "SEE THE BEST COURSE #3";
+  static const String onBoardingTitle4 = "SEE THE BEST COURSE #4";
+
+
+
+  static const String onBoardingSubTitle1="Tut app is a platform for learning and teaching";
+  static const String onBoardingSubTitle2="Tut app is a platform for learning and teaching";
+  static const String onBoardingSubTitle3="Tut app is a platform for learning and teaching";
+  static const String onBoardingSubTitle4="Tut app is a platform for learning and teaching";
+
+
+
+}
